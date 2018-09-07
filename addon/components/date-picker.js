46311a999a80ffb70edf3735ddb64d15ed34b64e
Oleg Grytsak <oleg.grytsak@promorepublic.com>
2018-09-07 14:09:26 +0300

    Saving progress to work home

--- addon/components/date-picker.js
+++ addon/components/date-picker.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import EmberObject, { computed } from '@ember/object';
 import layout from '../templates/components/date-picker';
 import { inject as service } from '@ember/service';
+import { compare } from '@ember/utils';
 
 export default Component.extend({
   layout,
@@ -10,31 +11,27 @@ export default Component.extend({
     this._super(...arguments);
 
     this.set('selectedDate', this.get('moment').moment());
-    // this.renderCalendar();
-
+    this.renderCalendar();
   },
 
   // Render calendar
   renderCalendar() {
     // Clear dayObjects array
-    this.set('dayObjects', []);
+    this.set('dayObjects', Ember.A());
     let
       // Clone moment object all next such variables with numbers are another clones of moment object
-      moment = this.get('selectedDate').clone(),
+      moment = this.get('selectedDate'),
       today = this.get('moment').moment(),
       // Get first day of first week
-      firstDay = moment.startOf('month').startOf('week'),
-      moment1 = this.get('selectedDate').clone(),
+      firstDay = moment.clone().startOf('month').startOf('week'),
       // Get last day of last week
-      lastDay = moment1.endOf('month').endOf('week'),
-      moment2 = this.get('selectedDate').clone(),
+      lastDay = moment.clone().endOf('month').endOf('week'),
       // Get previous month (number)
-      oldMonth = +moment2.subtract(1, 'months').month(),
-      moment3 = this.get('selectedDate').clone(),
+      oldMonth = +moment.clone().subtract(1, 'months').month(),
       // Get next month (number)
-      newMonth = +moment3.add(1, 'months').month(),
-      //  Get date picker element
-      datePicker = document.getElementById('ember-date-picker');
+      newMonth = +moment.clone().add(1, 'months').month();
+
+    if (!moment) return;
 
     // Fill array dayObjects with objects for each day
     while (firstDay.isBefore(lastDay)) {
@@ -47,34 +44,23 @@ export default Component.extend({
       day.set('oldDay', +firstDay.month() === oldMonth);
       day.set('newDay', +firstDay.month() === newMonth);
       day.set('startOfWeek', firstDay.format('dddd') === 'Sunday');
+      if (firstDay.isSame(this.get('selectedDate'))) {
+        day.set('active', true);
+      } else {
+        day.set('active', false);
+      }
       day.set('momentDateObj', firstDayClone);
 
       this.dayObjects.push(day);
       firstDay.add(1, 'days');
-
-      // let day = {
-      //   monthDay: firstDay.date(),
-      //   weekDay: firstDay.format('dddd'),
-      //   today: firstDay.isSame(today, 'day'),
-      //   oldDay: +firstDay.format('M') === oldMonth,
-      //   newDay: +firstDay.format('M') === newMonth,
-      //   startOfWeek: firstDay.format('dddd') === 'Sunday'
-      // };
-      // this.dayObjects.push(day);
-      // firstDay.add(1, 'days');
-
-
     }
-    // Change month name when toggle prev/next on month
+    // Change month and year when click prev/next
     this.set('currentMonth', this.get('selectedDate').format('MMMM'));
     this.set('currentYear', this.get('selectedDate').format('YYYY'));
-    // Show date picker
-    datePicker.classList.remove('hidden');
+
+    console.log('selectedDate', this.get('selectedDate'));
+    console.log('pickedDate', this.get('pickedDate'));
   },
-  numOfTimes: computed('times', function () {
-    const times = parseInt(this.get('times'));
-    return new Array(times);
-  }),
   // Array of objects for each day
   dayObjects: computed('selectedDate', function () {
     return [];
@@ -85,57 +71,78 @@ export default Component.extend({
   currentYear: computed('selectedDate', function () {
     return this.get('selectedDate').format('YYYY');
   }),
+  hours: computed('selectedDate', {
+    get() {
+      const selectedDate = this.get('selectedDate');
+
+      if (!selectedDate) return;
+
+      return selectedDate.hours();
+    },
+
+    set(propName, value) {
+      this.set('selectedDate', this.get('selectedDate').clone().hours(value));
+    }
+  }),
   actions: {
-    // Show date picker
-    toggleCalendar() {
-      this.renderCalendar();
-    },
-    // Hide date picker
-    hideCalendar() {
-      const datePicker = document.getElementById('ember-date-picker');
-      datePicker.classList.add('hidden');
-    },
+
     // Pick date when click on day in calendar picker
     pickDate(day) {
-      const datePicker = document.getElementById('date-picker-main-input');
-      datePicker.value = '';
-      datePicker.value = day.momentDateObj.format('YYYY-MM-DD');
-      // console.log('day.momentDateObj:', day.momentDateObj);
+      // Check if there is already active day and clear if there is
+      this.dayObjects.any(function (item) {
+        if (item.active) {
+          item.set('active', false);
+        }
+      });
+
+      day.set('active', true);
+      // this.set('pickedDate', day.momentDateObj);
+      this.set('selectedDate', day.momentDateObj);
     },
-    // Toggle previous month
-    togglePrev() {
-      this.get('selectedDate').subtract(1, 'months');
+
+    changeMonth(direction) {
+      if (direction === 1) {
+        this.get('selectedDate').add(1, 'months');
+      }
+      if (direction === -1) {
+        this.get('selectedDate').subtract(1, 'months');
+      }
       this.renderCalendar();
     },
-    // Toggle next month
-    toggleNext() {
-      this.get('selectedDate').add(1, 'months');
+
+    // Change year on buttons click
+    changeYear(direction) {
+      if (direction === 1) {
+        this.get('selectedDate').add(1, 'years');
+      }
+      if (direction === -1) {
+        this.get('selectedDate').subtract(1, 'years');
+      }
       this.renderCalendar();
     },
-    // Toggle next year
-    toggleYearUp() {
-      this.get('selectedDate').add(1, 'years');
-      this.renderCalendar();
-    },
-    // Toggle previous year
-    toggleYearDown() {
-      this.get('selectedDate').subtract(1, 'years');
-      this.renderCalendar();
-    },
-    // Toggle year on manual enter in input
+
+    // Change year on manual enter in input
     toggleYear() {
       const datePicker = document.getElementById('date-picker-year-input');
       let selectedDate = this.get('selectedDate');
 
+      if (datePicker.value.length > 4) {
+        datePicker.value = datePicker.value.slice(0, 4);
+      }
+
       if (datePicker.value.length === 4) {
-
-        if (datePicker.value.length > 4) {
-          datePicker.value = datePicker.value.slice(0, 4);
-        }
-
         this.set('selectedDate', this.get('moment').moment(`${datePicker.value}-${selectedDate.format('MM')}-${selectedDate.format('DD')}`));
         this.renderCalendar();
       }
+    },
+
+    changeHours(direction) {
+      const
+        selectedMoment = this.get('selectedDate'),
+        currentHours = selectedMoment.hours(),
+        newHours = (currentHours + 24 + direction) % 24;
+
+      this.set('hours', newHours);
     }
   }
 
